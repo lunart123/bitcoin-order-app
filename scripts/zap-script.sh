716278ed1e26cbab2c4c97890df0121c2c1e3bf9
QX <tqxtqx@gmail.com>
2024-07-16 15:53:14 +0800

    lint, dast,sast

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 docker pull owasp/zap2docker-stable
 docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://kenken64.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
