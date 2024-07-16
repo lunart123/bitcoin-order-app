c2aa17bad8b81ad1c352659750dbc4886f20c413
QX <tqxtqx@gmail.com>
2024-07-16 16:02:31 +0800

    lint, dast,sast

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,5 @@
-!/bin/bash
-
-docker pull owasp/zap2docker-stable
-docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://kenken64.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
+#!/bin/bash
+docker pull zaproxy/zap-stable
+docker run -i zaproxy/zap-stable zap-baseline.py -t "https://lunart123.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
